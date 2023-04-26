a044bbd1fd251ec69bf049178b2d79b2436992ed
Jana Krenn <krenn.jana.05@gmail.com>
2023-04-26 10:47:50 +0200

    Ausgabe

--- quicksort.java
+++ quicksort.java
@@ -66,7 +66,7 @@ class quicksort {
         for (int i = 0; i < size; i++)
             System.out.print(arr[i] + " ");
 
-        System.out.println();
+        System.out.println("Hallo");
     }
 
     // Driver Code
